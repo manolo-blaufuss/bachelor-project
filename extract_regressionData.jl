ad9ae5cebf122904a6f34c64fb1230351b752511
manolo-blaufuss <manolo.blaufuss@gmail.com>
2024-05-27 19:08:58 +0200

    remove code for testing

--- extract_regressionData.jl
+++ extract_regressionData.jl
@@ -62,7 +62,7 @@ Assign communication partners for each cell.
 function assign_communication_partners(n_cells::Int, n_groups::Int, communication_pairs::Vector{Any})
     communication_idxs = zeros(Int, n_cells)
     n_cells_per_group = n_cells ÷ n_groups
-    threshold = round(Int, 0.0 * n_cells_per_group)
+    threshold = round(Int, 0.7 * n_cells_per_group)
     for sel_communication in communication_pairs
         sender_group = sel_communication[1]
         receiver_group = sel_communication[2]
@@ -80,6 +80,7 @@ function assign_communication_partners(n_cells::Int, n_groups::Int, communicatio
             end
         end
     end
+    """
     println(count(x -> 251 <= x <= 500, communication_idxs[1:250]))
     println(count(x -> 501 <= x <= 750, communication_idxs[251:500]))
     println(count(x -> 751 <= x <= 1000, communication_idxs[501:750]))
@@ -89,6 +90,7 @@ function assign_communication_partners(n_cells::Int, n_groups::Int, communicatio
         comm_mat[i, communication_idxs[i]] = 1
     end
     heatmap(comm_mat')
+    """
     return communication_idxs
 end
 """
