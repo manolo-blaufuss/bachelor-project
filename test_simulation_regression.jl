f70ea70591d561e4372f5c51ab9bb8b81c44181e
manolo-blaufuss <manolo.blaufuss@gmail.com>
2024-06-22 18:46:55 +0200

    VAE testing

--- test_simulation_regression.jl
+++ test_simulation_regression.jl
@@ -14,9 +14,10 @@ gr(dpi=300)
 ################################################
 # Initialize for (possible) dimension reduction:
 ################################################
-dim_reduction = "AE"    # "none", "AE", "VAE"
-AE_architecture = "deep"   # "simple", "deep"
+dim_reduction = "VAE"    # "none", "AE", "VAE"
+AE_architecture = "simple"   # "simple", "deep"
 latent_dim = 4
+encoder = Chain()
 ################################################
 
 # Generate the simulated data (set a seed for reproducibility):
@@ -25,6 +26,7 @@ dataset, group_communication_matrix, cell_group_assignments, receptor_genes, sel
 
 # Preprocess the data:
 X, Y = preprocess_data(dataset, group_communication_matrix, noise_percentage=0.3f0, save_figures=true)
+
 
 if dim_reduction in ["AE", "VAE"]
     Z_X, Z_Y = dimension_reduction(X, Y, dim_reduction, AE_architecture, latent_dim, save_figures=true)
